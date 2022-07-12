cbb2b4445911aa1d03e0e587c14231aad6d66a89
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-12 16:26:52 -0500

    Update Student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -27,6 +27,16 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
     
+  #Update Student
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to students_path
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def student_params
