dd233c274a2583a534bc08b37ee9274134a759ff
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-14 15:54:50 -0500

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_161411) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_204757) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "demos", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
