801d89b5c73e4effcea744549ea3de273e085009
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-14 10:23:18 -0500

    Add notice

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,37 +1,42 @@
 class StudentsController < ApplicationController
-  #DRY
+  #DRY Callback
   before_action :set_student, only: %i[show edit update destroy]
 
+  #Index, New, Create -> Collection Methods, id is not needed
+  #Listing data
   def index
     @students = Student.all
   end
 
+  #Instantiate a new resourse
   def new
     @student = Student.new
   end
 
+  #Saves data captured from 'new'
   def create
     @student = Student.new(student_params)
     #Redirect to student
     if @student.save
-      redirect_to students_path
+      redirect_to students_path, notice: "Student has been created successfully"
     else
       render :new
     end
   end
 
-  #Implementing show action
+  #Show, Edit, Update, Destroy -> Members Methods, id is needed
+  #Implementing show action, fetchting data on browers
   def show
   end
 
-  #Editing Student
+  #Editing Student, fetching resource from database
   def edit
   end
     
-  #Update Student
+  #Update Student, save new info database
   def update
     if @student.update(student_params)
-      redirect_to students_path
+      redirect_to students_path, notice: "Student has been updated successfully"
     else
       render :edit
     end
@@ -40,7 +45,7 @@ class StudentsController < ApplicationController
   #Destroy Student
   def destroy
     @student.destroy
-    redirect_to students_path
+    redirect_to students_path, notice: "Student has been deleted successfully"
   end
     
   private
